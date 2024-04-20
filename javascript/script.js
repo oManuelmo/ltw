89c437ff3441eb7522450b3e4daa7cc0584102bf
Manuel Mo <manuelmo.steam@gmail.com>
2024-04-20 22:50:17 +0100

    css divided

--- javascript/script.js
+++ javascript/script.js
@@ -20,6 +20,87 @@ if (searchArtist) {
     }
   })
 }
+
 function togglePopup(){
   document.getElementById("popup-1").classList.toggle("active");
 }
+
+function togglePopup(number){
+  if (number==1){
+    document.getElementById("popup-1").classList.toggle("active");
+  }
+  else if(number==2){
+    document.getElementById("popup-2").classList.toggle("active");
+  }
+}
+
+function tryy(){
+  const popup = document.getElementById("popup-1");
+const popupBtn = document.querySelector('#account-button');
+popupBtn.addEventListener('click', ()=> {
+  popup.classList.toggle("active");
+})
+document.addEventListener('click', e => {
+  if(popup.contains(e.target)!= e.target!== popupBtn){
+    popup.classList.delete('active');
+  }
+})
+}
+
+window.addEventListener("scroll", function() {
+  if (window.scrollY > 200) { // Change 100 to the scroll position where you want the element to appear
+    document.body.classList.add("scrolling");
+  } else {
+    document.body.classList.remove("scrolling");
+  }
+});
+
+
+
+// Initialize slide index
+let slideIndex = 1;
+
+// Display initial slide
+showSlides(slideIndex);
+
+// Function to show slides
+function showSlides(n) {
+  let i;
+  const slides = document.getElementsByClassName("highlighted-product");
+  const dots = document.getElementsByClassName("dot");
+  
+  // Loop through slides and hide them
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  
+  // Loop through dots and remove active class
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  
+  // Ensure slide index doesn't exceed slide count
+  if (n > slides.length) {
+    slideIndex = 1;
+  }    
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  
+  // Display the current slide and set corresponding dot as active
+  slides[slideIndex-1].style.display = "flex";  
+  dots[slideIndex-1].className += " active";
+}
+
+// Function to advance slides
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Function to set current slide
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+document.addEventListener('DOMContentLoaded', function() {
+  showSlides(slideIndex);
+});
