1f41b16fb32433c69e65bf4b3f26a63e8810282b
Manuel Mo <manuelmo.steam@gmail.com>
2024-04-21 18:18:16 +0100

    login page implemented

--- pages/loginPage.php
+++ pages/loginPage.php
@@ -10,7 +10,12 @@
     <div class="container">
         <img src="../docs/logo-removebg-preview.png" alt="Maets" class="logo">
         <h1>Login</h1>
-        <?php drawLoginForm(); ?>
+        <form action="login_process.php" method="post"> 
+            <input type="email" id="email" name="email" required placeholder="E-mail adress">
+            <input type="password" id="password" name="password" required placeholder="Password">
+            <button type="submit">CONTINUE</button>
+        </form>
+        <p>When you login, you will agree with our <a href="https://youtube.com/watch?v=dQw4w9WgXcQ">Politics of privacy</a>.</p>
     </div>
 </body>
 </html>
